404934d01875d717ac5a4c413e03c46bffcde1a6
node4 <node4@node5.successive.com>
2018-08-09 17:54:36 +0530

    add all files

---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let app = express();
+let fs = require('fs');
+app.use(express.json());
+let counter = 10;
+
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "database.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+ })
+
+ app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "database.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        // console.log(req.body.user);
+        data = Object.assign({},data,{["user"+counter++]:req.body.user});
+        // console.log( data );
+        fs.writeFile( __dirname + "/" + "database.json",JSON.stringify(data),function(error){
+            if(error){
+                console.log(error);
+            }
+        });
+        res.end( JSON.stringify(data));
+    });
+ })
+
+ app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "database.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+
+ var server = app.listen(8081, function () {
+ 
+   var host = server.address().address
+   var port = server.address().port
+ 
+   console.log("Example app listening at http://%s:%s", host, port)
+ 
+ })
